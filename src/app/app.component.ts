11e120f5815560a6f38f8fdc8931a81408dec453
burg_c <burg_c@git.epitech.eu>
2020-07-09 17:06:33 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { IpcRenderer } from 'electron';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-electron-test';
+
+  private ipc: IpcRenderer;
+
+  constructor(){
+    if ((<any>window).require) {
+      try {
+        this.ipc = (<any>window).require('electron').ipcRenderer;
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
+
+  openModal(){
+    console.log("Open a modal");
+    this.ipc.send("openModal");
+  }
+}
